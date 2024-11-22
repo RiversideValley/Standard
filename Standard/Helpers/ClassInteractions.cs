bbbcd2d7dfae5cbddfed9df5a481d97ac7b53e2c
Lamparter <notlamparter@outlook.com>
2024-11-22 16:40:23 +0000

    Add XML documentation to helpers

--- Standard/Helpers/ClassInteractions.cs
+++ Standard/Helpers/ClassInteractions.cs
@@ -4,8 +4,18 @@ using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace Riverside.Standard.Helpers
 {
+    /// <summary>
+    /// Provides helper methods for class interactions.
+    /// </summary>
     public static class ClassInteractions
     {
+        /// <summary>
+        /// Determines whether the specified type syntax derives from the specified base type name.
+        /// </summary>
+        /// <param name="typeSyntax">The type syntax to check.</param>
+        /// <param name="baseTypeName">The fully qualified name of the base type.</param>
+        /// <param name="semanticModel">The semantic model used to get symbol information.</param>
+        /// <returns><c>true</c> if the specified type syntax derives from the specified base type name; otherwise, <c>false</c>.</returns>
         public static bool DerivesFrom(TypeSyntax typeSyntax, string baseTypeName, SemanticModel semanticModel)
         {
             var typeSymbol = semanticModel.GetSymbolInfo(typeSyntax).Symbol as INamedTypeSymbol;
