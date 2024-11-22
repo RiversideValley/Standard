1d8d302691f6aff0583652ed834c6951e77d24d1
Lamparter <notlamparter@outlook.com>
2024-11-22 16:44:52 +0000

    Rename `Riverside.Runtime.Partial` to `Riverside.Runtime.Modern`

--- Modern/Class1.cs
+++ Modern/Class1.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
-namespace Riverside.Runtime.Partial
+namespace Riverside.Runtime.Modern
 {
     public class Class1
     {
